36aa4a8b9b807466283c8d47110d14c1ebe825ff
Conor Moran <{ID}+{username}@users.noreply.github.com>
2025-03-20 19:22:50 +0000

    Library overhaul with Classes

--- library.js
+++ library.js
@@ -1,115 +1,130 @@
-const myLibrary = [];
-
-const form = document.getElementById("form_container");
-
-const table_container = document.getElementById("table_container");
-
 class Book {
+
+    static #isInternalConstructing = false;
+
+    static form = document.getElementById("form_container");
+
+    static titleInput = document.getElementById("title");
+    static authorInput = document.getElementById("author");
+    static pagesInput = document.getElementById("pages");
+    static readInput = document.getElementById("read");
+
+    static #library = [];
+
     constructor(title, author, pages, read) {
+        if (!Book.#isInternalConstructing) {
+            throw new TypeError("Book is not constructable");
+
+        }
+        Book.#isInternalConstructing = false;
         this.title = title;
-        this.author = author
-        this.pages = pages
-        this.title = read
-    }
-}
-
-function addBookToLibrary() {
-    // Object to array handler
-    const book = new Book();
-
-
-
-    const title = document.getElementById("title");
-    const author = document.getElementById("author");
-    const pages = document.getElementById("pages");
-    const read = document.getElementById("read");
-
-    if (title.value == " " || author.value == "" || pages.value == "") {
-        alert("Ensure you input a value in both fields!");
-    } else {
-        // perform operation with form input
-        book.title = title.value;
-        book.author = author.value;
-        book.pages = pages.value;
-        book.read = read.options[read.selectedIndex].value;
-        myLibrary.push(book);
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
 
-    // -----------------------
+    static #checkForm() {
+        if (this.titleInput.value == "" || this.authorInput.value == "" || this.pagesInput.value == "") {
+            alert("Ensure you input a value in all fields!");
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-    // Book to client handler
-    const info_container = document.createElement("div");
+    static addBookToLibrary(title, author, pages, read) {
+        if (!Book.#checkForm()) { return; };
+        Book.#isInternalConstructing = true;
+        const book = new Book(title, author, pages, read);
+        this.#library.push(book);
+        console.log(`Added Book: ${title}`);
+        Book.#displayBooks(book);
+    }
 
-    info_container.classList.add("information-element-container");
-    table_container.append(info_container);
+    static #displayBooks(book) {
+        const table_container = document.getElementById("table_container");
 
-    for (let i = 0; i < 3; i++) {
-        const field_text = document.createElement("div");
-        field_text.classList.add("field-text");
+        const info_container = document.createElement("div");
+        info_container.classList.add("information-element-container");
+        table_container.append(info_container);
 
-        info_container.append(field_text);
-        field_text.textContent = Object.values(book)[i];
-        if (i == 2) {
+        for (const prop in book) {
             const field_text = document.createElement("div");
             field_text.classList.add("field-text");
-
+            field_text.textContent = book[prop];
             info_container.append(field_text);
 
-            const read_button = document.createElement("button");
-            read_button.classList.add("read-button");
-            if (book.read == "Read") { read_button.classList.add("green-button"); } else { read_button.classList.remove("green-button"); }
-            read_button.textContent = book.read;
+            if (book[prop] == book.read) {
+                this.#readButton(book, info_container);
+                field_text.remove();
+            }
+        }
+    }
 
+    static #readButton(book, info) {
+        const info_container = info;
+        const field_container = document.createElement("div");
+        const button_container = document.createElement("div");
+        const read_button = document.createElement("button");
 
-            const buttons_container = document.createElement("div");
-            buttons_container.classList.add("buttons-container");
+        field_container.classList.add("field-text");
+        button_container.classList.add("buttons-container");
+        read_button.classList.add("read-button");
 
-            field_text.append(buttons_container);
+        info_container.append(field_container);
+        field_container.append(button_container);
+        button_container.append(read_button);
 
-            buttons_container.append(read_button);
+        read_button.textContent = book.read;
 
-            const delete_button = document.createElement("button");
-            delete_button.classList.add("close-button");
+        switch (book.read) {
+            case "Read":
+                read_button.classList.add("green-button");
+                break;
+            case "Not Read":
+                read_button.classList.remove("green-button");
+                break;
+        }
 
-            let devImage = new Image();
-            devImage.src = "images/trash-can-outline.svg";
-            devImage.classList.add("delete-icon")
-            delete_button.append(devImage);
+        const delete_button = document.createElement("button");
+        delete_button.classList.add("close-button");
+        button_container.append(delete_button);
+        let devImage = new Image();
+        devImage.src = "images/trash-can-outline.svg";
+        devImage.classList.add("delete-icon");
+        delete_button.append(devImage);
 
-            buttons_container.append(delete_button);
-
-            console.log(Object.keys(myLibrary));
-
-            delete_button.addEventListener("mouseup", (e) => {
-                console.log(myLibrary);
-                for (let i = 0; i < myLibrary.length; i++) {
-                    if (myLibrary[i] == book) {
-                        myLibrary.splice(i, 1);
-                        document.querySelectorAll(".information-element-container")[i].remove();
-                    }
+        delete_button.addEventListener("mouseup", () => {
+            info_container.remove();
+            for (let i = 0; i < this.#library.length; i++) {
+                if (this.#library[i] == book) {
+                    this.#library.splice(i, 1);
+                    console.log(`Removed book: ${book.title}`);
                 }
-            })
+            }
+        });
 
-            read_button.addEventListener("mouseup", (e) => {
-                if (book.read == "Read") {
+        read_button.addEventListener("mouseup", () => {
+            switch (book.read) {
+                case "Read":
                     read_button.textContent = "Not Read";
                     book.read = "Not Read";
                     read_button.classList.remove("green-button");
-                } else {
+                    break;
+                case "Not Read":
                     read_button.textContent = "Read";
                     book.read = "Read";
                     read_button.classList.add("green-button");
-                }
-            })
-        }
+                    break;
+            }
+        });
+
     }
+
 }
 
-
-
-form.addEventListener("submit", (event) => {
+Book.form.addEventListener("submit", (event) => {
     event.preventDefault();
-
-    addBookToLibrary();
-    form.reset();
-})
+    Book.addBookToLibrary(Book.titleInput.value, Book.authorInput.value, Book.pagesInput.value, Book.readInput.options[Book.readInput.selectedIndex].value);
+    Book.form.reset();
+});
